505d75354e4213abbcca29cd1a98767351c00fcc
kyawzinhtet2001 <105270445+kyawzinhtet2001@users.noreply.github.com>
2022-05-12 15:57:26 +0630

    added bunch of files

--- calculator_function.php
+++ calculator_function.php
@@ -24,7 +24,27 @@ function mod($a,$b) : int{
 
 
 
-echo dividetoend(1,2);
-echo add(1,2);
-echo multiply(1,2);
-echo substract(1,2);
+// echo dividetoend(1,2);
+// echo add(1,2);
+// echo multiply(1,2);
+// echo substract(1,2);
+
+if(isset($_POST['submit'])){
+    $a=$_POST['a'];
+    $b=$_POST['b'];
+
+    switch($_POST['mode']){
+        case "add":
+            echo add($a,$b);
+            break;
+        case "mul":
+            echo multiply($a,$b);
+            break;
+        case "sub":
+            echo substract($a,$b);
+            break;
+        case "mod":
+            echo mod($a,$b);
+            break;
+    }
+}
